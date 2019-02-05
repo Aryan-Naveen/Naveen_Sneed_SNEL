62af304894a60a81662bf4dbe841d00b13cb9d32
Aryan-Naveen <36176381+Aryan-Naveen@users.noreply.github.com>
2019-02-05 14:58:22 -0500

    Add files via upload

--- Generate_random_words.py
+++ Generate_random_words.py
@@ -1,10 +1,12 @@
 import random
 
 j=0
-text = ""
+i=0
 
-file = open('file.txt', 'w')
-for j in range (18000):
-    text += chr(random.randint(32, 126))
+for i in range(100):
+    text = ""
+    file = open('file' + str(i) + '.txt', 'w')
+    for j in range (18000):
+        text += chr(random.randint(32, 126))
 
-file.write(text)
+    file.write(text)
